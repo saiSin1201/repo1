c0cdbe0bec8e7e241b7ef8abde05f22a5a55b475
saishin <893169270@qq.com>
2020-12-29 06:17:02 +0900

    添加springboot mod1

---
+++ springboot_mod1/src/main/java/com/itheima/springboot_mod1/SpringbootMod1Application.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_mod1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMod1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMod1Application.class, args);
+	}
+
+}
---
+++ springboot_mod1/src/test/java/com/itheima/springboot_mod1/SpringbootMod1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_mod1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMod1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
